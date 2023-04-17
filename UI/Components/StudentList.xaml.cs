b60addada13a9373018ea8d05fda8b1583d604a5
Damyan <damyan1mihov@gmail.com>
2023-04-17 16:38:17 +0300

    upr 5

---
+++ UI/Components/StudentList.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using DataLayer.Database;
+
+namespace UI.Components;
+
+public partial class StudentList : UserControl
+{
+    public StudentList()
+    {
+        InitializeComponent();
+        using (var context = new DatabaseContext())
+        {
+            var records = context.Users.ToList();
+            students.DataContext = records;
+        }
+    }
+}
--- UI/Windows/MainWindow.xaml.cs
+++ UI/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DataLayer.Database;
 
 namespace UI
 {
@@ -23,6 +24,7 @@ namespace UI
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
